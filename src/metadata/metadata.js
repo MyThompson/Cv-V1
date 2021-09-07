bc4d778c0e208113fe4969b0fef4e90e0a6b2ca8
Mythompson <mythompson@bsu.edu>
2021-09-06 21:35:12 -0400

    edit to template and metadata

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -8,64 +8,27 @@ module.exports = {
     //             </a>`,
     'Email': '<a href="mailto:Mythompson@bsu.edu">Mythompson@bsu.edu</a>'
   },
-    Education: [
+    education: [
       "Bachelors of Computer Science (anticipated)",
       "Bachelors of Fine Arts: Animation (anticipated)"
-    ]
-//   skills: [
-//     ['Comedy', '++++'],
-//     ['Love', '++++'],
-//     ['Theater', '++++'],
-//     ['Tragedy', '++++'],
-//     ['Poetry', '++++'],
-//     ['Erotic', '+++'],
-//     ['Sonnets', '++++'],
-//     ['Plays', '++++'],
-//     ['Writing', '++++'],
-//     ['Directing', '+++'],
-//     ['Literature', '++++'],
-//     ['Influence', '++++'],
-//   ],
-//   plays: [
-//     {
-//       title: 'Hamlet',
-//       written: 'Likely early 17th century',
-//       badges: ['tragedy', 'philosophy', 'death'],
-//       contents: `
-// Prince Hamlet is visited by his father's ghost and ordered to avenge his father's murder by killing King Claudius, his uncle. After struggling with several questions, including whether what the ghost said is true and whether it is right for him to take revenge, Hamlet, along with almost all the other major characters, is killed.
-// `
-//     },
-//     {
-//       title: 'Twelfth Night',
-//       written: '1600 – 1601',
-//       badges: ['comedy', 'love'],
-//       contents: `
-// Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
-// `
-//     },
-//     {
-//       title: 'The Tragedy of Romeo and Juliet',
-//       written: '1595–1596, with a possible early draft written in 1591',
-//       badges: ['tragedy', 'death', 'love'],
-//       contents: `
-// In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
-// `
-//     }
-//   ],
-//   experience: [
-//     {
-//       header: 'Performance',
-//       skills: ['theater', 'play'],
-//       contents: `
-// Thousands of performances of my plays have been staged since the end of the 16th century. Many of my greatest plays were performed by the Lord Chamberlain's Men and King's Men acting companies at the [Globe](https://en.wikipedia.org/wiki/Globe_Theatre) and Blackfriars Theatres.
-// `
-//     },
-//     {
-//       header: 'Poetry',
-//       skills: ['poetry', 'erotic'],
-//       contents: `
-// In 1593 and 1594, when the theatres were closed because of plague, I published two narrative poems on sexual themes, Venus and Adonis and The Rape of Lucrece. He dedicated them to Henry Wriothesley, Earl of Southampton.
-// `
-//     }
-//   ]
+    ],
+    experience: [
+      "Internship - High Alpha : Docket"
+    ],
+  skills: [
+    {
+            title: 'Full Stack Development and Software Engineering',
+            website: '(github)',
+            languages:['Javascript', "React", "Python", "Java", 'C/C++/C#'],
+            familiarsSoftware:[],
+          },
+    {
+            title: 'Animation and Art',
+            website: '(artstation)',
+            mediums:['3D animation and modeling','2D animation', 'Digital Illustration', 'Traditional Art'],
+            software:["Adobe Suite", "Maya", "Blender", "ZBrush", "Toonboom Harmony"]
+    }
+  ]
+
+  
 };
