88274f5ff9d2073f019d62c10e5c6818f3c67a7c
Mythompson <mythompson@bsu.edu>
2021-09-07 23:16:29 -0400

    check

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -31,18 +31,9 @@ module.exports = {
   ],
   experience: [
     {
-      header: 'Performance',
-      skills: ['theater', 'play'],
-      contents: `
-Thousands of performances of my plays have been staged since the end of the 16th century. Many of my greatest plays were performed by the Lord Chamberlain's Men and King's Men acting companies at the [Globe](https://en.wikipedia.org/wiki/Globe_Theatre) and Blackfriars Theatres.
-`
-    },
-    {
-      header: 'Poetry',
-      skills: ['poetry', 'erotic'],
-      contents: `
-In 1593 and 1594, when the theatres were closed because of plague, I published two narrative poems on sexual themes, Venus and Adonis and The Rape of Lucrece. He dedicated them to Henry Wriothesley, Earl of Southampton.
-`
+      header: 'Developer Intern',
+      contents: `Five week internship at Docket, a portfolio company of High Alpha`,
+      date:"7/2019 - 8/2018"
     }
   ]
 
