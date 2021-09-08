1c11948ec6abe657605485955e5271a3c9dbdd71
Mythompson <mythompson@bsu.edu>
2021-09-08 03:52:28 -0400

    fickle framework fixed

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -2,19 +2,17 @@ module.exports = {
   name: 'Mercedes Thompson',
   title: 'Software Developer, Animator',
   facts: {
-    // 'Homepage': `<a href="https://en.wikipedia.org/wiki/William_Shakespeare">
-    //               <span class="print">https://en.wikipedia.org/wiki/William_Shakespeare</span>
-    //               <span class="screen">wiki</span>
-    //             </a>`,
+    'Homepage': `<a href="https://en.wikipedia.org/wiki/William_Shakespeare">
+                  <span class="print">https://en.wikipedia.org/wiki/William_Shakespeare</span>
+                  <span class="screen">wiki</span>
+                </a>`,
     'Email': '<a href="mailto:Mythompson@bsu.edu">Mythompson@bsu.edu</a>'
   },
   education: [
       "Bachelors of Computer Science (anticipated)",
       "Bachelors of Fine Arts: Animation (anticipated)"
     ],
-    // experience: [ foo
-    //   "Internship - High Alpha : Docket"
-    // ],
+ 
   experience: [
     {
       header: 'Developer Intern',
@@ -27,8 +25,8 @@ module.exports = {
             title: 'Full Stack Development and Software Engineering',
             website: '(github)',
             languages:['Javascript', "React", "Python", "Java", 'C/C++/C#', 'sql'],
-            familiarsSoftware:["placeholder"],
-          },
+            familiarsSoftware:["placeholder"]
+    },
     {
             title: 'Animation and Art',
             website: '(artstation)',
@@ -40,23 +38,24 @@ module.exports = {
     {
       project:'Port Call Simulation',
       languages: ['python', 'sql'],
-      description:'Exercise consisting of using simulated data to create a fully functioning backend from the ground up. The system consists of a database comprised of several related data sets, of which were then communicated and manipulated with and upper level set of DAOS and queries that are available to be parsed through a front end.',
+      description:'Exercise consisting of using simulated data to create a fully functioning backend from the ground up. The system consists of a database comprised of several related data sets, of which were then communicated and manipulated with and upper level set of DAOS and queries that are available to be parsed through a front end.'
     },
     {
       project:'Simulated Authentication System',
       languages: ['HTML', 'Javascript'],
-      description:'A frontend starting at a login landing page. The cache temporatily holds onto sign up information, and then the user can login with credentials and their emplayment role, latter of which routes to alternate pages. This can also use ouside JSON to suppliment.',
+      description:'A frontend starting at a login landing page. The cache temporatily holds onto sign up information, and then the user can login with credentials and their emplayment role, latter of which routes to alternate pages. This can also use ouside JSON to suppliment.'
     },
     {
       project:'Chatter',
       languages: ['ReactJS'],
-      description:'Dynamic messaging software connected to an API'},
+      description:'Dynamic messaging software connected to an API'
+    }
   ],
 
   hobbies: [
       'Cooperative table top games',
       'Playing Violin',
-      'Creative Writing, Worldbuilding, and Ecological Design',
+      'Creative Writing, Worldbuilding, and Ecological Design'
   ],
   
 
