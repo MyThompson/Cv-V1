6dc8ba2d396b56dc1674eaae920e53e260117c3d
Mythompson <mythompson@bsu.edu>
2021-09-08 02:36:57 -0400

    added information

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -8,18 +8,25 @@ module.exports = {
     //             </a>`,
     'Email': '<a href="mailto:Mythompson@bsu.edu">Mythompson@bsu.edu</a>'
   },
-    education: [
+  education: [
       "Bachelors of Computer Science (anticipated)",
       "Bachelors of Fine Arts: Animation (anticipated)"
     ],
     // experience: [ foo
     //   "Internship - High Alpha : Docket"
     // ],
+  experience: [
+    {
+      header: 'Developer Intern',
+      contents: `Five week internship at Docket, a portfolio company of High Alpha`,
+      date:"7/2019 - 8/2018"
+    }
+  ],
   skills: [
     {
             title: 'Full Stack Development and Software Engineering',
             website: '(github)',
-            languages:['Javascript', "React", "Python", "Java", 'C/C++/C#'],
+            languages:['Javascript', "React", "Python", "Java", 'C/C++/C#', 'sql'],
             familiarsSoftware:["placeholder"],
           },
     {
@@ -29,13 +36,29 @@ module.exports = {
             software:["Adobe Suite", "Maya", "Blender", "ZBrush", "Toonboom Harmony"]
     }
   ],
-  experience: [
+  projects: [
     {
-      header: 'Developer Intern',
-      contents: `Five week internship at Docket, a portfolio company of High Alpha`,
-      date:"7/2019 - 8/2018"
-    }
-  ]
+      project:'Port Call Simulation',
+      languages: ['python', 'sql'],
+      description:'Exercise consisting of using simulated data to create a fully functioning backend from the ground up. The system consists of a database comprised of several related data sets, of which were then communicated and manipulated with and upper level set of DAOS and queries that are available to be parsed through a front end.',
+    },
+    {
+      project:'Simulated Authentication System',
+      languages: ['HTML', 'Javascript'],
+      description:'A frontend starting at a login landing page. The cache temporatily holds onto sign up information, and then the user can login with credentials and their emplayment role, latter of which routes to alternate pages. This can also use ouside JSON to suppliment.',
+    },
+    {
+      project:'Chatter',
+      languages: ['ReactJS'],
+      description:'Dynamic messaging software connected to an API'},
+  ],
+
+  hobbies: [
+      'Cooperative table top games',
+      'Playing Violin',
+      'Creative Writing, Worldbuilding, and Ecological Design',
+  ],
+  
 
   
 };
